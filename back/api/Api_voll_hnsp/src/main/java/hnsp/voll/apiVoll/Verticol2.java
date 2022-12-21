3969c7ae41fde453040a43240a04d2ccc7bdf2f4
JCabral <cabralzay@gmail.com>
2022-12-21 22:33:55 +0100

    segmento de estrutura de projecto BACKEND api 211222

--- back/api/Api_voll_hnsp/src/main/java/hnsp/voll/apiVoll/Verticol2.java
+++ back/api/Api_voll_hnsp/src/main/java/hnsp/voll/apiVoll/Verticol2.java
@@ -72,10 +72,63 @@ public class Verticol2  extends AbstractVerticle {
       return Future.succeededFuture();
     }
 
-    private void allFunction(RouterBuilder routerBuilder) {
+    private void allFunction(RouterBuilder router) {
 
       System.out.println("AQUI VAMOS A LLAMAR TODOS LOS ENDPOINT ==>>");
+
+      router.operation("createvoll").handler(this::createvoll);
+
+      /*
+      router.operation("getAllVoll").handler(this::getAllVoll);
+      router.operation("updateVoll").handler(this::updateVoll);
+      router.operation("deleteVoll").handler(this::deleteVoll);
+      router.operation("getVollById").handler(this::getVollById);
+      router.operation("createResidencia").handler(this::createResidencia);
+      router.operation("getAllResindeciaVoll").handler(this::getAllResindeciaVoll);
+      router.operation("updateResidencia").handler(this::updateResidencia);
+      router.operation("deleteResidencia").handler(this::deleteResidencia);
+      router.operation("findResidenciaById").handler(this::findResidenciaById);
+      router.operation("createEspecialidad").handler(this::createEspecialidad);
+      router.operation("getAllEspecialidad").handler(this::getAllEspecialidad);
+      router.operation("updateEspecialidad").handler(this::updateEspecialidad);
+      router.operation("deleteEspecialdiad").handler(this::deleteEspecialdiad);
+      router.operation("findEspecailidadById").handler(this::findEspecailidadById);
+      router.operation("createPeriodoVoll").handler(this::createPeriodoVoll);
+      router.operation("getAllPeriodo").handler(this::getAllPeriodo);
+      router.operation("updatePeriodo").handler(this::updatePeriodo);
+      router.operation("deteletePeriodo").handler(this::deteletePeriodo);
+      router.operation("getPerdiodoById").handler(this::getPerdiodoById);
+      router.operation("createPayVoll").handler(this::createPayVoll);
+      router.operation("getAllPay").handler(this::getAllPay);
+      router.operation("createSeccion").handler(this::createSeccion);
+      router.operation("getAllSeccion").handler(this::getAllSeccion);
+      router.operation("updatePay").handler(this::updatePay);
+      router.operation("deletePay").handler(this::deletePay);
+      router.operation("getPayById").handler(this::getPayById);
+      router.operation("updateSeccion").handler(this::updateSeccion);
+      router.operation("deleteSeccion").handler(this::deleteSeccion);
+      router.operation("getSeccionById").handler(this::getSeccionById);
+      router.operation("getDataAllVoll").handler(this::getDataAllVoll);
+      router.operation("getDataOnlyVoll").handler(this::getDataOnlyVoll);
+      router.operation("uploadDoc").handler(this::uploadDoc);
+      router.operation("getImg").handler(this::getImg);
+      router.operation("donloadDoc").handler(this::donloadDoc);
+      router.operation("updatePassword").handler(this::updatePassword);
+      router.operation("setPassword").handler(this::setPassword);
+      router.operation("login").handler(this::login);
+      router.operation("statusSession").handler(this::statusSession);
+      router.operation("updateSession").handler(this::updateSession);
+      router.operation("logout").handler(this::logout);
+
+      */
+
+
+
+
     }
+
+  private void createvoll(RoutingContext routingContext) {
+  }
 
 
   public static void sendError(
